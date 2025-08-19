cb37389b9069b948ceb91c6321cafa46889ebb6b
Devakumaran27 <devakumaran40@gmail.com>
2025-08-19 21:07:55 +0530

    Commit

---
+++ C2TC_Core_Java_Assignement/src/com/devakumaran/assignement2/student.java
@@ -0,0 +1,9 @@
+package com.devakumaran.assignement2;
+
+public class student {
+	//Default Constructor  
+		public student() {
+		        System.out.println("Student object is created");
+		    }
+
+}
